926480b3143f559513f9ab8819d0dac90ef58e08
drawdlu <152858099+drawdlu@users.noreply.github.com>
2024-11-02 21:43:02 +0800

    Pass all tests

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -16,6 +16,52 @@ module Enumerable
     end
     new_arr
   end
+
+  def my_all?
+    self.my_each do |element|
+      return false unless yield(element)
+    end
+    true
+  end
+
+  def my_any?
+    self.my_each do |element|
+      return true if yield(element)
+    end
+    false
+  end
+
+  def my_none?
+    self.my_each do |element|
+      return false if yield(element)
+    end
+    true
+  end
+
+  def my_count
+    return self.length unless block_given?
+    count = 0
+    self.my_each do |element|
+      count += 1 if yield(element)
+    end
+    count
+  end
+
+  def my_map
+    new_arr = []
+    self.my_each do |element|
+      new_arr << yield(element)
+    end
+    new_arr
+  end
+
+  def my_inject(initial)
+    val = initial
+    self.my_each do |element|
+      val = yield(val, element)
+    end
+    val
+  end
 end
 
 # You will first have to define my_each
